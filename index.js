2a854bcd5cb20842b939cbc994322ce4f547adf6
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-26 11:06:26 +0530

    updating

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 const app =express();
 
-//advance routing
 //string pattern path
 app.get("/ab?cd",(req,res)=>{
     res.send("if user hit(acd) or (abcd) then this will run");
