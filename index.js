97dc9e01b3b73e3b6f2fcab590842c0a004812aa
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-26 10:53:03 +0530

    practicing basic routing

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ app.get('/',  (req,res) =>{
     res.send("home")
 });
 
+//more routes
+app.get('/about',  (req,res) =>{
+    res.send("<h1>ABOUT <h1>")
+});
+
+//adding contact route
+app.get('/contact',  (req,res) =>{
+    res.send("<h1>CONTACT<h1>")
+});
+
 app.listen(8000,()=> console.log("server up!"));
