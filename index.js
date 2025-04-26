b77068d4efda42ca969a9f40bd105764ff69b573
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-26 10:49:39 +0530

    creating basic routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 const app =express();
 
 //adding basic routing
-
+app.get('/',  (req,res) =>{
+    res.send("home")
+});
 
 app.listen(8000,()=> console.log("server up!"));
