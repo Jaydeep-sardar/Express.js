52441f7c853e366be4b49130f443ee97deb1e710
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-26 11:02:27 +0530

    advance routing

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 import express from "express";
 const app =express();
 
-//adding basic routing
-app.get('/',  (req,res) =>{
-    res.send("home")
-});
-
-//more routes
-app.get('/about',  (req,res) =>{
-    res.send("<h1>ABOUT <h1>")
-});
-
-//adding contact route
-app.get('/contact',  (req,res) =>{
-    res.send("<h1>CONTACT<h1>")
+//advance routing
+//string pattern path
+app.get("/ab?cd",(req,res)=>{
+    res.send("if user hit(acd) or (abcd) then this will run");
 });
 
 app.listen(8000,()=> console.log("server up!"));
