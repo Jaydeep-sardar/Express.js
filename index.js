72e51839d9f21363d3103131682b38da33b24cb0
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 10:49:22 +0530

    regex

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 const app = express();
 
 // route using regex
-app.get(/^\/ab?cd$/, (req, res) => {
-    res.send("if user hits (acd) or (abcd), this will run");
+app.get(/x/,(req,res) => {
+    res.send("if the path includes the letter x it will work");
 });
 
 app.listen(8000, () => console.log("server up!"));
