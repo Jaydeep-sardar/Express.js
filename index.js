863546072106796e4aa848030dfed28e268a4dc5
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 11:40:28 +0530

    routes params

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import students from "./routes/student.js";
-import teachers from "./routes/teacher.js"
 const app = express();
+//route params
+app.get('/student/delete/:id',(req,res)=>{
+    res.send(req.params.id);
+});
 
-app.use("/teachers",teachers);
-app.use ("/students",students);
 app.listen(8000, () => console.log("server up!"));
