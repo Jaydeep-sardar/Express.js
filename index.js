af755107c49a94ccbf06877afca28e1c5d3f24af
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 10:52:19 +0530

    nested routes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
 const app = express();
 
-// route using regex
-app.get(/x/,(req,res) => {
-    res.send("if the path includes the letter x it will work");
+app.get("/products/iphone",(req,res)=>{
+    res.send("this will work if u provide  /products/iphone");
 });
 
 app.listen(8000, () => console.log("server up!"));
