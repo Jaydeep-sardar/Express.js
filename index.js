2686dfd6bc6b9077561076ea9b5485fd8308f3ff
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 11:03:44 +0530

    array of callback function

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
 const app = express();
 
-app.get("/double-cb",
-    (req,res,next)=>{
-        console.log("first callback");
-        next();
-    },
-    (req,res)=>{
-        res.send("second callback");
-    }
-);
+const cb1 =(req,res,next)=>{
+    console.log("first callback");
+    next()
+};
+
+const cb2 =(req,res,next)=>{
+    console.log("second callback");
+    next()
+};
+
+const cb3 =(req,res)=>{
+    console.log("third callback");
+    res.send ("array of callbacks");
+};
+
+app.get("/array-cb",[cb1,cb2,cb3]);
 app.listen(8000, () => console.log("server up!"));
