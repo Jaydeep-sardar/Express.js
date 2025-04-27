8efc5128d1e67121fbf560458cb942cc399d9621
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 11:42:40 +0530

    examples of route params

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 app.get('/student/delete/:id',(req,res)=>{
     res.send(req.params.id);
 });
+app.get('/ecom/products/iphone/:model',(req,res)=>{
+    res.send("iphone" ,req.params.model);
+});
 
 app.listen(8000, () => console.log("server up!"));
