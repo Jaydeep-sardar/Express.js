fa296bba6ac9719039c2bd3b15f330a2a0d01985
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-27 11:48:00 +0530

    more examples

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ app.get('/ecom/products/iphone/:model',(req,res)=>{
     res.send("iphone" ,req.params.model);
 });
 
+/* app.params("id",(req,res,next,id)=>{
+    console.log(`id :${id}`);
+    });
+    app.get("/user/:id",(req,res)=>{
+        console.log("this is user is path");
+        res.send("response ok");
+        }); */
+
 app.listen(8000, () => console.log("server up!"));
