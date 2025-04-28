37661626b1dd0b71c175cfddeec26c9e4b44eae3
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-28 11:40:01 +0530

    updating

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ import express from "express";
 import products from "./products,js";
 const app = express();
 
-//query string
-app.get('/product',(req,res)=>{
-    res.send(`response OK ${req.query.category}`);
-});
-
 //sending json
 app.get('/products',(req,res)=>{
     res.json(products)
