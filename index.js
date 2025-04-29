6676c80fbd1e27f6516f40ac708f37f53529fbc3
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-29 11:13:29 +0530

    completion of practicing express.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import route from"./routes/route.js"
 const app = express();
 
 app .set ('view engine' ,"ejs")
+
 app.use(express.static(join(process.cwd(),"public")))
+
+
 app.use("/",route);
 app.listen(8000, () => console.log("server up!"));
